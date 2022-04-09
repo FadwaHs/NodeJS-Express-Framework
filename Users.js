3815845d94933f2325294b9137b3d7e00c5c9cab
Fadwa Hsissou <51267601+FadwaHs@users.noreply.github.com>
2022-04-09 15:54:32 +0000

    Add files via upload

---
+++ Users.js
@@ -0,0 +1,50 @@
+const users = [
+    {
+      id:1,
+      name:"fadwa",
+      email:"fadwa@gmail.com"
+
+    },
+    {
+        id:2,
+        name:"imad",
+        email:"imad@gmail.com"
+  
+      },
+
+      {
+        id:3,
+        name:"amal",
+        email:"amal@gmail.com"
+  
+      },
+      {
+        id:4,
+        name:"chaimae",
+        email:"chaime@gmail.com"
+  
+      },
+
+      {
+        id:5,
+        name:"zineb",
+        email:"zineb@gmail.com"
+  
+      },
+      {
+        id:6,
+        name:"amina",
+        email:"amina@gmail.com"
+  
+      },
+
+      {
+        id:7,
+        name:"amine",
+        email:"amine@gmail.com"
+  
+      },
+
+];
+
+module.exports = users;
